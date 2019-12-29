5dea0b6b131f6b6c9d84d218a46e581b32a2eb5d
Majid <a@yahoo.com>
2019-12-29 13:52:28 +0330

    First complete version. One data file for every month. Every line in the file is for each year, and price change is at the end of each line.

--- main.py
+++ main.py
@@ -1,19 +1,60 @@
 import os.path
 import io
+import datetime
 #----------------------------------------------------
-def find_entries(input_file,i):
+class candle:
+    def __init__(self):
+        self.date = datetime.datetime(2019,12,29)
+        self.open = "0.0"
+        self.high = "0.0"
+        self.low = "0.0"
+        self.close = "0.0"
+        self.volume = "0"
+#----------------------------------------------------
+def find_entries(input_file,m):
     
-    if(not isinstance(i,int) or not isinstance(input_file,io.IOBase)):
+    if(not isinstance(m,int) or not isinstance(input_file,io.IOBase)):
         print("Err: Callong function \"find_entries(...)\" with {} input parameter".format(type(i)))
         return
-    fn = "Month"+str(i)+".txt"
+    fn = "Month"+str(m)+".txt"
     try:
         output_file = open(fn,"w")
     except Exception as exp:
         print(exp)
         return
+
+    output_file.write("{:^20s}{:^15s}{:^15s}{:^15s}{:^15s}{:^15s}{:^15s}\n".
+    format("Time","Open","High","Low","Close","Volume","Change"))
+    cdl = candle()
     for line in input_file:
-        print(line)
+        index2 = line.find(",")
+        index1 = index2 + 1
+        index2 = line.find(",",index1)
+        date_str = line[0:index2]
+        #print(date_str)
+        cdl.date = datetime.datetime.strptime(date_str,"%Y.%m.%d,%H:%M")
+
+        index1 = index2 + 1
+        index2 = line.find(",",index1)
+        cdl.open = line[index1:index2]
+
+        index1 = index2 + 1
+        index2 = line.find(",",index1)
+        cdl.high = line[index1:index2]
+        index1 = index2 + 1
+        index2 = line.find(",",index1)
+        cdl.low = line[index1:index2]
+        index1 = index2 + 1
+        index2 = line.find(",",index1)
+        cdl.close = line[index1:index2]
+        index1 = index2 + 1
+
+        l = len(line)
+        cdl.volume = line[index1:l-1]
+
+        if cdl.date.month == m:
+            output_file.write("{:20s}{:^15s}{:^15s}{:^15s}{:^15s}{:^15s}{:^13.2f}\n".
+            format(str(cdl.date),cdl.open,cdl.high,cdl.low,cdl.close,cdl.volume,float(cdl.close)-float(cdl.open)))
 
     
 #----------------------------------------------------
