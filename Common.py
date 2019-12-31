fdd9da26d6b2dabdf83d0392d089a91084c34460
Majid <a@yahoo.com>
2019-12-31 14:17:26 +0330

    Stable point1, the probability of following candle to be in the same direction of the current candle, is calculated

--- Common.py
+++ Common.py
@@ -1,13 +1,20 @@
 import datetime
+TimeFrame = 30
+ListSize = int((24*60)/30)
 #----------------------------------------------------
 class candle:
     def __init__(self):
         self.date = datetime.datetime(2019,12,29)
-        self.open = "0.0"
-        self.high = "0.0"
-        self.low = "0.0"
-        self.close = "0.0"
-        self.volume = "0"
+        self.open = 0
+        self.high = 0
+        self.low = 0
+        self.close = 0
+        self.volume = 0
+#----------------------------------------------------
+class ctr:
+    def __init__(self):
+        self.total_ctr = 0
+        self.ctr = 0
 #----------------------------------------------------
 def get_candle_data(line,cdl):
     
@@ -23,19 +30,25 @@ def get_candle_data(line,cdl):
 
     index1 = index2 + 1
     index2 = line.find(",",index1)
-    cdl.open = line[index1:index2]
+    cdl.open = float(line[index1:index2])
 
     index1 = index2 + 1
     index2 = line.find(",",index1)
-    cdl.high = line[index1:index2]
-    index1 = index2 + 1
-    index2 = line.find(",",index1)
-    cdl.low = line[index1:index2]
-    index1 = index2 + 1
-    index2 = line.find(",",index1)
-    cdl.close = line[index1:index2]
-    index1 = index2 + 1
+    cdl.high = float(line[index1:index2])
 
+    index1 = index2 + 1
+    index2 = line.find(",",index1)
+    cdl.low = float(line[index1:index2])
+
+    index1 = index2 + 1
+    index2 = line.find(",",index1)
+    cdl.close = float(line[index1:index2])
+
+    index1 = index2 + 1
     l = len(line)
-    cdl.volume = line[index1:l-1]
+    cdl.volume = int(line[index1:l-1])
 #----------------------------------------------------
+def find_time_index(time=datetime.datetime):
+    index = time.hour *60 + time.minute
+    index /= TimeFrame
+    return(int(index))
--- main.py
+++ main.py
@@ -4,10 +4,44 @@ import datetime
 import Common
 #----------------------------------------------------
 def process():
+    time_frame_list = [Common.ctr() for i in range(0,Common.ListSize)]
+    
     input_file = open("XAUUSD.cfd30.csv","r")
-    cdl = candle()
+    cdl1 = Common.candle()
+    
+    line = input_file.readline()
+    Common.get_candle_data(line,cdl1)
     for line in input_file:
-        get_candle_data(line,cdl)
+        index = Common.find_time_index(cdl1.date)
+
+        cdl2 = Common.candle()
+        Common.get_candle_data(line,cdl2)
+        
+        delta_t = cdl2.date - cdl1.date
+
+        if(delta_t.seconds == Common.TimeFrame*60):
+            time_frame_list[index].total_ctr += 1
+            if(cdl1.close > cdl1.open):
+                if(cdl2.close > cdl2.open):
+                    time_frame_list[index].ctr += 1
+            if(cdl1.close < cdl1.open):
+                if(cdl2.close < cdl2.open):
+                    time_frame_list[index].ctr += 1
+        del cdl1
+        cdl1 = cdl2
+        del cdl2
+        
+        
+    output_file = open("Out.txt","w")
+    for i in range(0,Common.ListSize):
+        output_file.write("{:0>2d}:{:0>2d}  Total={:^6d} , Count={:^6d} , Probability={:>6.2f}%\n"
+        .format(int((i*Common.TimeFrame)/60),int((i*Common.TimeFrame)%60),time_frame_list[i].total_ctr,time_frame_list[i].ctr,
+        100*time_frame_list[i].ctr/time_frame_list[i].total_ctr))
+
+    output_file.close()
+
+        
+        
 
     input_file.close()
 #----------------------------------------------------
@@ -17,7 +51,7 @@ try:
     if(os.path.exists(".\XAUUSD.cfd30.csv") == True):
         process()
     else:
-        Print("Input file not found!")
+        print("Input file not found!")
 except Exception as exp:
     print(exp)
 #----------------------------------------------------
